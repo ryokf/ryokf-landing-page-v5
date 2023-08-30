3c2f0a5a524fedd7ba2eeae21cbe9b8fbe520f61
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-08-30 10:10:25 +0700

    install dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+'use client';
+
+import { Button, Modal } from 'flowbite-react'; 
+
+function DefaultModal() {
+  const [openModal, setOpenModal] = useState();
+  const props = { openModal, setOpenModal };
+
+  return (
+    <>
+      <Button onClick={() => props.setOpenModal('default')}>Toggle modal</Button>
+      <Modal show={props.openModal === 'default'} onClose={() => props.setOpenModal(undefined)}>
+        <Modal.Header>Terms of Service</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+              companies around the world are updating their terms of service agreements to comply.
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to
+              ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as
+              possible of high-risk data breaches that could personally affect them.
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => props.setOpenModal(undefined)}>I accept</Button>
+          <Button color="gray" onClick={() => props.setOpenModal(undefined)}>
+            Decline
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,13 +48,13 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+     
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-6xl'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +66,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+    <DefaultModal></DefaultModal>
+
     </>
   )
 }
